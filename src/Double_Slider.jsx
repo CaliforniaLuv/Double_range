f6fc181a19a2bb42a620aa234a5901d70004a486
CaliforniaLuv <caifornialove.96@gmail.com>
2022-10-10 19:32:46 +0900

    add: range step 적용

--- src/Double_Slider.jsx
+++ src/Double_Slider.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import "./Double_Slider.css"
 
 function Double_Slider() {
+
+  const [range, setRange] = useState({
+    left: 0,
+    right: 100
+  })
+
+  const handleLeft = (e) => {
+    setRange({...range, left: e.target.value})
+  }
+
+  const handleRight = (e) => {
+    setRange({...range, right: e.target.value})
+  }
+
   return (
     <>
+     <div className="values">
+        <span className="left-value">{range.left} </span>
+          -
+          <span className="right-value"> {range.right}</span>
+        </div>
       <main className="container">
         <div className="slider-track">
-          <input type="range" min="0" max="100"  defaultValue="30" id="Double-left" />
-          <input type="range" min="0" max="100"  defaultValue="70" id="Double-right" />
+          <input type="range" min="0" max="100" step="11.11"  id="Double-left" value={range.left} onChange={handleLeft}/>
+          <input type="range" min="0" max="100" step="11.11" id="Double-right" value={range.right} onChange={handleRight}/>
         </div>
       </main>
     </>
